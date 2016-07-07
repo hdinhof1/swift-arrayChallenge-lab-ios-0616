ca05e5dbd26ee56b502e68916bf7de95fd2f4418
hdinhof1 <hdinhof1@binghamton.edu>
2016-07-07 10:48:48 -0400

    All tests pass, has UITest cannot be found error

--- ArrayChallenge/ViewController.swift
+++ ArrayChallenge/ViewController.swift
@@ -15,11 +15,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-    
+        let quantity = [6, 4, 12, 4]
+        let itemNames = ["Bananas", "Apples", "Eggs", "Rolls"]
         
+        shoppingList = makeShoppingList(itemNames, quantityOfItems: quantity)
+        print (shoppingList)
         
     }
-    
+    func makeShoppingList(foodNames: [String], quantityOfItems: [Int]) -> [String] {
+        var list : [String] = []
+        for (index, item) in foodNames.enumerate() {
+            list.append("\(index + 1). \(quantityOfItems[index]) \(item)")
+        }
+        return list
+    }
+
     
     
 }
